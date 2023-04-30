5e263f5409ef681bae5240714d941d5cf34307bf
bryce <SwPGameDev@gmail.com>
2023-04-29 21:42:10 -0600

    Added test scene "BRYCE CAR", car generator script, and test car model

---
+++ Assets/Objects/Obstacles/CarSpawner.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] carTypes;
+    public GameObject carParent;
+    public GameObject[] carPosList;
+
+
+    private IEnumerator Start()
+    {
+        carParent = GameObject.Find("CarsParent");
+        carPosList = new GameObject[4];
+        for (int i = 0; i < carPosList.Length; i++)
+        {
+            carPosList[i] = GameObject.Find("pos" + (i));
+        }
+
+
+
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f); // Waits one second
+
+            int difficulty = UnityEngine.Random.Range(0, 3); // Randomly chooses difficulty
+            // List<int> availableLanes = new() { 0, 1, 2, 3 }; // create a list of available lanes
+
+            List<int> availableLanes = new List<int>
+            {
+                0,
+                1,
+                2,
+                3
+            };
+
+
+
+            print("Difficulty: " + difficulty);
+
+            for (int i = 0; i < difficulty; i++)
+            {
+                int carTypeDecider = UnityEngine.Random.Range(0, carTypes.Length); // Randomly chooses car model
+                int laneIndex = UnityEngine.Random.Range(0, availableLanes.Count); // get a random index from the available lanes
+                int laneDecider = availableLanes[laneIndex]; // choose the lane at that index
+                availableLanes.RemoveAt(laneIndex); // remove the chosen lane from the list
+                print("Lane: " + laneDecider);
+                print("Car Type: " + carTypeDecider);
+
+
+
+                GameObject chosenLane = carPosList[laneDecider];
+                Vector3 clonedCarPos = new Vector3(chosenLane.transform.position.x, 0, chosenLane.transform.position.z);
+                Instantiate(carTypes[carTypeDecider], clonedCarPos, Quaternion.identity, carParent.transform);
+            }
+
+            //GameObject clonedCar = Instantiate(carTypes[carTypeDecider], carPosList[laneDecider].transform.position, carPosList[laneDecider].transform.rotation);
+
+            //Vector3 SpawnPosition = new Vector3(pos0x, 0, pos0z);
+            //Instantiate(carTypes[carTypeDecider], SpawnPosition, Quaternion.identity, carParent.transform);
+
+
+        }
+    }
+}
